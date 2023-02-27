4bef797d19199230bd44a9d73bb1b85ed6305d47
Sheel2007 <62717403+Sheel2007@users.noreply.github.com>
2023-02-27 14:12:15 -0500

    Re-Uploaded Problem B

---
+++ VCU Problem Solutions/2016/B.py
@@ -0,0 +1,35 @@
+"""
+# sample input
+3
+0123
+5890
+4444
+
+#sample output
+8765
+5430
+9999
+
+"""
+
+iterations = int(input())
+
+input_data = []
+
+for i in range(1, iterations+1):
+	input_data.append(input())
+
+
+for i in input_data:
+	data = []
+	for j in i:
+		j = (int(j) + 5) % 10
+	
+		data.append(j)
+	
+	data[0], data[3] = data[3], data[0]
+	data[1], data[2] = data[2], data[1]
+	answer = ""
+	for num in data:
+		answer += str(num)
+	print(answer)
