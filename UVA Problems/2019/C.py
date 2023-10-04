11f932a51ca988f7bbf5b4d13e9ea80ec1eb2b56
Sheel2007 <62717403+Sheel2007@users.noreply.github.com>
2023-10-04 09:52:31 -0400

    Completed Solution to C.py

--- UVA Problems/2019/C.py
+++ UVA Problems/2019/C.py
@@ -1,6 +1,11 @@
-def calculate(vi, theta, x):
-    pass
+import math
 
+def calculate(vi, theta, x):
+    vx = vi * math.cos(theta)
+    vy = vi * math.sin(theta)
+
+    t = x / vx
+    return (vy * t) - 1 /2 * 9.8 * (t ** 2)
 
 test_cases = int(input())
 
@@ -8,4 +13,6 @@ for _ in range(test_cases):
 
     vi, theta, x = list(map(float, input().split()))
 
-    calculate(vi, theta, x)
+    number = calculate(vi, theta, x)
+
+    print(math.floor(number * 1000) / 1000) # rounds down
